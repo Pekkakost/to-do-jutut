4240431ebe23d87aa18847df10a472db9cb21c24
pekka <pekkaj.kostiainen@gmail.com>
2023-09-09 20:20:58 +0300

    mitä lie tulossa

--- appi/PÄÄharkka/gui.py
+++ appi/PÄÄharkka/gui.py
@@ -4,6 +4,6 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
-window = sg.Window('My_To-Do App', layout=[[label], [input_box, add_button]])
+window = sg.Window('My_To-Do App', layout=[[label, input_box, add_button]])
 window.read()
 window.close()
