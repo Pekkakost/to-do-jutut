20e708eeb7521d192567b5ac47997d4cae1e0cb2
pekka <pekkaj.kostiainen@gmail.com>
2023-09-10 11:06:42 +0300

    implement add button

--- appi/PÄÄharkka/functionspp.py
+++ appi/PÄÄharkka/functionspp.py
@@ -10,7 +10,7 @@ def write_todos(todos_arg, filepath=FILEPATH):
     with open(filepath, "w") as file:
         file.writelines(todos_arg)
 
-print("i am outside")
+
 if __name__=="__main__":
     print("hello")
     print(get_todos())
